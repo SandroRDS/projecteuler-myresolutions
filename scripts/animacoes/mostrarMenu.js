bad587da87b983cf4491dcbef0fa8e421371b952
SandroRDS <sandror.desouza847@gmail.com>
2022-10-04 20:36:05 -0300

    Criando novo layout e seções da página home

--- scripts/animacoes/mostrarMenu.js
+++ scripts/animacoes/mostrarMenu.js
@@ -1,7 +1,19 @@
-var botaoMenu = document.querySelector(".cabecalho__menu");
+var seta = document.querySelector(".cabecalho__navegacao__seta");
 var menu = document.querySelector(".cabecalho__navegacao");
+var escondido = true;
 
-botaoMenu.onclick = function()
+seta.onclick = function()
 {
     menu.classList.toggle("cabecalho__navegacao--recolhido");
+    
+    if(escondido)
+    {
+        seta.innerHTML = "navigate_next";
+    }
+    else
+    {
+        seta.innerHTML = "chevron_left";
+    }
+
+    escondido = !escondido;
 }
