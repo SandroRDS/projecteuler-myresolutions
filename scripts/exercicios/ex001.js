6187980e6abf88b0cc22d3de0e3ec9646ced8707
SandroRDS <sandror.desouza847@gmail.com>
2022-10-08 11:39:50 -0300

    Adicionando saídas de console no exercício 1

--- scripts/exercicios/ex001.js
+++ scripts/exercicios/ex001.js
@@ -1,9 +1,24 @@
 var soma = 0;
 
-for(var i = 3; i < 1000; i++)
+function iniciarAlgoritmo()
 {
-    if((i%3 == 0) || (i%5 == 0))
+    for(var i = 3; i < 1000; i++)
     {
-        soma += i;
+        console.log(`i = ${i} | soma = ${soma}`);
+
+        if((i%3 == 0) || (i%5 == 0))
+        {
+            console.log(`${i} é múltiplo de 3 ou 5`);
+            soma += i;
+            console.log(`soma = ${soma - i} + ${i} = ${soma}`);
+        }
+        else
+        {
+            console.log(`${i} não é múltiplo de 3 ou 5`);
+        }
+
+        console.log("--------------------");
     }
+
+    console.log(`Resultado Final = ${soma}`)
 }
