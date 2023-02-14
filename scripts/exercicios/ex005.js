e4236a06e5cdb4c952e7c9c223cc7db017642c06
SandroRDS <sandror.desouza847@gmail.com>
2023-02-14 00:26:46 -0300

    Aprimorando algoritmo do ex005

--- scripts/exercicios/ex005.js
+++ scripts/exercicios/ex005.js
@@ -1,26 +1,72 @@
-var menorNumero;
-var numeroEncontrado = false, divisivel = true;
-var i = 20;
+//Problema 5 - Menor múltiplo
 
-while(!numeroEncontrado)
-{
-    for(var j = 1; j <= 20; j++)
+let primoAtual = 2;
+
+const buscarNumeroPrimo = () => {
+    let primo = true;
+    primoAtual += primoAtual !== 2 ? 2 : 1;
+
+    for(let i = 3; i < primoAtual; i += 2)
     {
-        if(i % j != 0)
+        if(primoAtual%i === 0)
         {
-            divisivel = false;
+            primo = false;
             break;
         }
     }
 
-    if(divisivel)
+    if(!primo)
     {
-        numeroEncontrado = true;
-        menorNumero = i;
-    }
-    else
-    {
-        i++;
-        divisivel = true;
+        buscarNumeroPrimo();
     }
 }
+
+const montarArraySequencial = (inicio, fim) => {
+    const array = [];
+
+    for(let i = inicio; i <= fim; i++)
+    {
+        array.push(i);
+    }
+
+    return array;
+}
+
+const encontrarMMC = (numeros) => {
+    let divisores = 1;
+    
+    while(!numeros.every(numero => numero === 1))
+    {
+        let algumDivisivel = false;
+    
+        numeros = numeros.map((numero) => {
+            if(numero % primoAtual === 0)
+            {
+                algumDivisivel = true;
+                return numero / primoAtual;
+            }
+            else
+            {
+                return numero;
+            }
+        });
+
+        console.log(numeros);
+
+        if(!algumDivisivel)
+        {
+            buscarNumeroPrimo();
+        }
+        else
+        {
+            divisores *= primoAtual;
+        }
+    }
+
+    return divisores;
+}
+
+const array = montarArraySequencial(1, 20);
+const menorMultiplo = encontrarMMC(array);
+
+console.log("Menor Múltiplo: " + menorMultiplo);
